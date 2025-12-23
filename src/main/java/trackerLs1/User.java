9f69f04edfbe05f0ff0c35f1d1e1a7d112d43467
Gerik228 <dimaakulichd6618@gmail.com>
2025-12-23 14:10:56 +1000

    Трекер Добавлен класс User.

--- src/main/java/trackerLs1/User.java
+++ src/main/java/trackerLs1/User.java
@@ -1,6 +1,15 @@
 package trackerLs1;
 
 public class User {
+    private int emplId;
+    private String fio;
+    private String post;
+    private String department;
 
+
+    public User (int emplId, String fio, String post, String department){
+
+
+    }
 
 }
