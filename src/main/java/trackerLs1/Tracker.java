60c15a96c127e415503828f4765fa1450ab40372
Gerik228 <dimaakulichd6618@gmail.com>
2025-12-23 15:16:54 +1000

    Трекер Отображение функционала создание методов в Tracker

--- src/main/java/trackerLs1/Tracker.java
+++ src/main/java/trackerLs1/Tracker.java
@@ -1,47 +1,33 @@
 package trackerLs1;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class Tracker {
-    private static final String sp = " ";
-    private static final String nl = "\n";
+public final class Tracker {
 
-    private ArrayList<Item> tasksList;
-
-
-    public static void main(String[] args) {
-
-    /*    System.out.println("Добрый день!"+ nl +
-                        "Я приложение трекер. " + nl +
-                        "На данный момент я умею: " + nl +
-                        "Создавать" + nl +
-                        "Редактировать" + nl +
-                        "Удалять" + nl +
-                        "Сортировать" + nl +
-                        "Искать таски по" + nl + " 1. имени" + nl +" 2. описанию" + nl + " 3. id" + nl+ " 4. дате создания");
+    private final List<Item> tasks = new ArrayList<>();
+    private final List<String> menu = new ArrayList<>();
 
 
-     */
+    private Tracker() {initMenu();}
 
-
-
-
-
-
+    private static class Holder {
+        private static final Tracker INSTANCE = new Tracker();
     }
 
+    public static Tracker getInstance() {return Holder.INSTANCE;}
 
+    private void initMenu() {
+        menu.add("1. Показать список");
+        menu.add("2. Создать задачу");
+        menu.add("3. Поиск задач");
+        menu.add("4. Удалить задачу");
+        menu.add("10. Завершить работу");
+    }
 
+    public void showMenu() {menu.forEach(System.out::println);}
 
+    public void showItems (){
 
-
-
-
-
-
-
-
-
-
-
+    }
 }
---
+++ src/main/java/trackerLs1/UiTracker.java
@@ -0,0 +1,13 @@
+package trackerLs1;
+
+public class UiTracker {
+
+    public static void main(String[] args) {
+
+       Tracker tracker =  Tracker.getInstance();
+        tracker.showMenu();
+
+    }
+
+
+}
