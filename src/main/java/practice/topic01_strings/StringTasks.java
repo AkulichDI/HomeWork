7c71b6efac37baabb9045d794f8736156444a111
Gerik228 <dimaakulichd6618@gmail.com>
2025-12-24 14:52:19 +1000

    * УРОК 2 — compressRLE

--- src/main/java/practice/topic01_strings/StringTasks.java
+++ src/main/java/practice/topic01_strings/StringTasks.java
@@ -4,16 +4,48 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-/* ТЕМА 1 — ЗАДАЧА 1.1: normalizeName(String s)
- *
-         * ЧТО ДЕЛАЕТ:
-        * Приводит имя к нормальному виду:
-        * - убирает пробелы по краям
- * - первая буква -> заглавная
- * - остальные буквы -> строчные
- */
+
 public class StringTasks {
 
+    public static String compressRLE(String s) {
+        if (validatorNotNull(s))return "Строка null";
+        StringBuilder sb = new StringBuilder();
+        int counter = 0;
+        s = s.trim();
+        char current = s.charAt(0);
+        for (int i = 1; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if (current == ch){
+                counter++;
+            }else {
+                sb.append(current).append(counter);
+                current = ch;
+                counter = 1;
+            }
+        }
+        return sb.toString();
+    }
+
+
+
+    public static boolean validatorNotNull(String s){
+        if (s == null)return true;
+        s = s.trim();
+        if (s.isEmpty())return true;
+        return false;
+    }
+
+
+    public static String repeat(String s, int n) {
+        if(StringTasks.validatorNotNull(s))throw new IllegalArgumentException("И что мне делать с пустой строкой ?!");
+        if( n < 0 ) throw new IllegalArgumentException("N < 0");
+        if( n == 0 ) return "";
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < n ; i++){
+            result.append(s);
+        }
+        return result.toString();
+    }
 
     public static boolean isPalindrome(String s) {
        if (s == null )return false;
@@ -40,7 +72,6 @@ public class StringTasks {
        return true;
     }
 
-
     public static int countVowels(String s) {
         if (s == null)return 0;
         List<Character> charsRUEN = new ArrayList<>();
@@ -50,7 +81,6 @@ public class StringTasks {
         for (var lit : chars.toCharArray()){
             charsRUEN.add(lit);
         }
-
         for (var lit : s.toCharArray()){
             for (var arrLit : charsRUEN) {
                 if (Character.toLowerCase(lit) == Character.toLowerCase(arrLit)){
@@ -58,20 +88,9 @@ public class StringTasks {
                     resultChars.add(lit);
                 }
             }
-
-
         }
-
-
      return counter;
     }
-
-
-
-
-
-
-
 
     public static String normName(String s){
         if(s == null){throw new IllegalArgumentException("Пустое имя");}
@@ -81,8 +100,10 @@ public class StringTasks {
     }
     public static void main(String[] args) {
 
+        System.out.println(StringTasks.compressRLE("wertyuiolkjhgfdssghjkhtrewrtyujhgf"));
 
-        System.out.println(StringTasks.countVowels("DIMA"));
+        // System.out.println(StringTasks.repeat("", 1));
+        //  System.out.println(StringTasks.countVowels("DIMA"));
         //System.out.println(StringTasks.isPalindrome("Aboba"));
         //System.out.println(StringTasks.normName("DIMA"));
     }
