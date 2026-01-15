b8fbd4ae77d130cc9fd146b4a69b7945fe816ba1
Gerik228 <dimaakulichd6618@gmail.com>
2026-01-16 09:04:34 +1000

    CodingBat 1.6

---
+++ src/main/java/CodingBat/warmup1/Makes10.java
@@ -0,0 +1,18 @@
+package CodingBat.warmup1;
+
+public class Makes10 {
+   /*
+    * Даны два целых числа a и b. Верните true, если одно из них равно 10 или если их сумма равна 10.
+    * makes10(9, 10) → true
+    * makes10(9, 9) → false
+    * makes10(1, 9) → true
+    */
+
+    public boolean makes10(int a, int b) {
+        if ( (a == 10 || b == 10) || a + b == 10 ) return true;
+        return false;
+    }
+
+
+
+}
