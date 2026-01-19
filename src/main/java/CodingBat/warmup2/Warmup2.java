ce13581c6429cbcf18e6e14fb9e990a1573d0fef
Gerik228 <dimaakulichd6618@gmail.com>
2026-01-19 14:36:05 +1000

    CodingBat 2.6

--- src/main/java/CodingBat/warmup2/Warmup2.java
+++ src/main/java/CodingBat/warmup2/Warmup2.java
@@ -108,5 +108,24 @@ public String stringTimes(String str, int n) {
         return  result;
     }
 
+/*
+    Для непустой строки, например «Code», верните строку, например «CCoCodCode».
 
+    stringSplosion("Code") → "CCoCodCode"
+    stringSplosion("abc") → "aababc"
+    stringSplosion("ab") → "aab"
+*/
+
+    public String stringSplosion(String str) {
+
+        String result = "";
+
+        for (int i = 0; i < str.length() ; i++) {
+
+            result += str.substring(0,i);
+
+        }
+
+        return result + str;
+    }
 }
