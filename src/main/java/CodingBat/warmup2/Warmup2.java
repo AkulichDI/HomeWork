0fdf0345edf7fa52fac137b1a730e2aff6696c76
Gerik228 <dimaakulichd6618@gmail.com>
2026-01-19 12:56:44 +1000

    CodingBat 2.4

--- src/main/java/CodingBat/warmup2/Warmup2.java
+++ src/main/java/CodingBat/warmup2/Warmup2.java
@@ -69,8 +69,22 @@ public String stringTimes(String str, int n) {
         return count;
     }
 
+/*
+    Для заданной строки верните значение true, если за первым вхождением символа «x» в строке сразу следует другой символ «x».
+    doubleX("axxbb") → true
+    doubleX("axaxax") → false
+    doubleX("xxxxx") → true
+*/
 
+    boolean doubleX(String str) {
 
+        int i = str.indexOf("x");
+        if (i == -1) return false;
+
+        // Is char at i+1 also an "x"?
+        if (i+1 >= str.length()) return false;
+        return str.substring(i+1, i+2).equals("x");
+    }
 
 
 }
