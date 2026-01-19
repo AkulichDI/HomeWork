a76bfeea68a1ae94e89b02162fe912d49e4c4d92
Gerik228 <dimaakulichd6618@gmail.com>
2026-01-19 16:05:57 +1000

    CodingBat 2.12

--- src/main/java/CodingBat/warmup2/Warmup2.java
+++ src/main/java/CodingBat/warmup2/Warmup2.java
@@ -248,13 +248,15 @@ public String stringTimes(String str, int n) {
     public String stringX(String str) {
 
         String result = "";
+        for (int i=0; i<str.length(); i++) {
 
-        for (int i = 1; i < str.length() - 1 ; i++) {
-            if (str.charAt(i) == 'x') continue;
-            result+=str.charAt(i);
-
+            if (!(i > 0 && i < (str.length()-1) && str.substring(i, i+1).equals("x"))) {
+                result = result + str.substring(i, i+1);
+            }
         }
         return result;
+
+
     }
 
 
