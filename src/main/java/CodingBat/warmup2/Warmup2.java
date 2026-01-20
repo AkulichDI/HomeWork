15466e54804e23aa4e460c49ced95dfe34a6118a
Gerik228 <dimaakulichd6618@gmail.com>
2026-01-20 16:24:00 +1000

    CodingBat 2.15

--- src/main/java/CodingBat/warmup2/Warmup2.java
+++ src/main/java/CodingBat/warmup2/Warmup2.java
@@ -323,8 +323,10 @@ public String stringTimes(String str, int n) {
 
         for (int i = 0; i < nums.length - 1; i++) {
 
-            if ( nums[i] == 6 && nums[i+1] == 6 || nums[i] == 6 && nums[i+1] == 7){
-                result++;
+            if ( nums[i] == 6){
+                if (nums[i+1] == 6 || nums[i+1] == 7){
+                    result++;
+                }
             }
         }
 
