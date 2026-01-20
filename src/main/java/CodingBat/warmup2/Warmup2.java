8a6fe17a3f5c9f77575757fb852dbfde52bd09c9
Gerik228 <dimaakulichd6618@gmail.com>
2026-01-20 15:20:51 +1000

    CodingBat 2.13

--- src/main/java/CodingBat/warmup2/Warmup2.java
+++ src/main/java/CodingBat/warmup2/Warmup2.java
@@ -261,4 +261,30 @@ public String stringTimes(String str, int n) {
 
 
 
+/*
+    Для заданной строки верните строку, состоящую из символов по индексам 0, 1, 4, 5, 8, 9 ... так, чтобы из «kittens» получилось «kien».
+
+    altPairs("kitten") → "kien"
+    altPairs("Chocolate") → "Chole"
+    altPairs("CodingHorror") → "Congrr"
+*/
+
+    public String altPairs(String str) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i += 4) {
+            sb.append(str.charAt(i));
+            if (i + 1 < str.length()) {
+                sb.append(str.charAt(i + 1));
+            }
+        }
+
+        return sb.toString();
+
+
+    }
+
+
+
 }
