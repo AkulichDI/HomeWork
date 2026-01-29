1a31ef8fb6ebe6788230d80aa6bf3c9346e9ba07
Gerik228 <dimaakulichd6618@gmail.com>
2026-01-29 14:40:19 +1000

    CodingBat 6.03

--- src/main/java/CodingBat/string/String2.java
+++ src/main/java/CodingBat/string/String2.java
@@ -34,5 +34,27 @@ public class String2 {
 
     }
 
+/*
+    Верните true, если строки «cat» и «dog» встречаются в заданной строке одинаковое количество раз.
+
+    catDog("catdog") → true
+    catDog("catcat") → false
+    catDog("1cat1cadodog") → true
+*/
+
+    public boolean catDog(String str) {
+
+        int dogCount = 0, catCount = 0;
+
+        for (int i = 0; i < str.length()-2; i++) {
+
+            if (str.startsWith("cat",i)) catCount++;
+            if (str.startsWith("dog",i)) dogCount++;
+
+        }
+        return catCount == dogCount;
+    }
+
+
 
 }
