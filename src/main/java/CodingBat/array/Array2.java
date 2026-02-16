6b4414f53992df3c65c44599c3aed253738df7a1
Gerik228 <dimaakulichd6618@gmail.com>
2026-02-16 15:47:21 +1000

    CodingBat 7.02

--- src/main/java/CodingBat/array/Array2.java
+++ src/main/java/CodingBat/array/Array2.java
@@ -18,8 +18,28 @@ public class Array2 {
         }
         return counter;
     }
+/*
+    Для массива из 1 или более целых чисел верните разницу между наибольшим и наименьшим значениями в массиве.
+    Примечание: встроенные методы Math.min(v1, v2) и Math.max(v1, v2) возвращают меньшее или большее из двух значений.
 
+    bigDiff([10, 3, 5, 6]) → 7
+    bigDiff([7, 2, 10, 9]) → 8
+    bigDiff([2, 10, 7, 2]) → 8
+ */
 
+    public int bigDiff(int[] nums) {
+        if ( nums.length < 2 ) return 0;
+        if ( nums.length == 2 ) return Math.abs(nums[0] - nums[1]);
+        int max = nums[0];
+        int min = nums[1];
+        min = Math.min(max, min);
+        max = Math.max(max, min);
+        for (int i = 1; i < nums.length ;  i++) {
+            min = Math.min(nums[i], min);
+            max = Math.max(max, nums[i]);
+        }
+        return max - min;
+    }
 
 
 
