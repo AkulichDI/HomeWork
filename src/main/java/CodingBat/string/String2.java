d69a863c8a918e1beca29e7c6ebcbb285a258683
Gerik228 <dimaakulichd6618@gmail.com>
2026-02-16 12:39:42 +1000

    CodingBat 6.18

--- src/main/java/CodingBat/string/String2.java
+++ src/main/java/CodingBat/string/String2.java
@@ -291,4 +291,23 @@ public class String2 {
 
     }
 
+/*
+        Возвращает true, если для каждого символа '*' (звездочки) в строке символы, стоящие непосредственно до и после звездочки, совпадают.
+
+        sameStarChar("xy*yzz") → true
+        sameStarChar("xy*zzz") → false
+        sameStarChar("*xa*az") → true
+ */
+
+    public boolean sameStarChar(String str) {
+
+        for (int i = 1; i < str.length() - 1; i++) {
+            if (str.charAt(i) == '*' && str.charAt(i - 1) != str.charAt(i + 1)) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
 }
